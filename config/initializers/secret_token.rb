175998f078552b5a190cf69270f81f04e6e2b4d6
vagrant <vagrant@precise32.(none)>
2016-07-20 23:33:49 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TeamQuiz::Application.config.secret_key_base = 'bd4d0ef4f28c68e644dd9bf106342d0e432fdde5bee235cc53db841fa21db26262d98e2f4d685d9086ae5924bfd51afd1195422664e3bc9630713b4377622a42'
